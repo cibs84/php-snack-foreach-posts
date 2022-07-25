c51b38a142cfe73d3825f60438b9b43875a3c20e
cibs84 <d.tilesi@gmail.com>
2022-07-25 12:03:42 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+<?php foreach ($posts as $postDate => $posts_list) { ?>
+    <?php foreach ($posts_list as $postInfo) { ?> 
+
+    <h2>Scritto da <?php echo $postInfo["author"]; ?> in data <?php echo $postDate; ?> </h2>
+    <h3><?php echo $postInfo["title"]; ?></h3>
+    <p><?php echo $postInfo["text"]; ?></p>
+    <hr>
+    
+    <?php } ?>
+<?php } ?>
+
+</body>
+</html>
